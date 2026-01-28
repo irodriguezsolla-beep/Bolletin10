b1885f0d30647e55015b95f0d58d00548731905f
Isaac <irodriguezsolla@danielcastelao.org>
2026-01-28 14:16:06 +0100

    Ejer2

--- boletin10ejer2.py
+++ boletin10ejer2.py
@@ -9,7 +9,7 @@ class Data:
         if type(ano) == int:
                  if int(ano)>= 1970 or int(ano)<= 2999:
                      int(ano)
-                     self.ano = ano
+                     self.__ano = ano
                  else:
                     raise DataError(f"El ano {ano} no es valido, tiene que ser mayor o igual a 1970, o menor o igual a 2999")
         else:
@@ -20,47 +20,54 @@ class Data:
     def setMes(self,mes):
         if type(mes) == int:
                 if int(mes) >= 1 or int(mes) <= 12:
-                    self.mes = mes
+                    self.__mes = mes
                 else:
                     raise DataError(f"El mes no puedete tener no puede ser menor de 1 o menor 12")
         else:
             raise TypeError(f"El formato de mes {type(mes)} y no es entero")
 
-    def eBisesto(self):
-        return (self.ano % 4 == 0 and self.ano % 100 != 0) or (self.ano % 400 == 0)
+    def getMes(self):
+        return self.__mes
 
     def setDia(self, dia):
-        if not isinstance(dia, int):
-            raise DataError("O día debe ser un enteiro")
-        # Días máximos por mes
-        dias_mes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        # Febreiro en ano bisesto
-        if self.mes == 2 and self.eBisesto():
-            max_dia = 29
-        else:
-            max_dia = dias_mes[self.mes - 1]
-        if dia < 1 or dia > max_dia:
-            raise DataError(f"O día {dia} non é válido para o mes {self.mes} no ano {self.ano}")
-        self.dia = dia
+        if type(dia)== int:
+            meses_31_dias = {1, 3, 5, 7, 8, 10, 12}
+            meses_30_dias = {4, 6, 9, 11}
 
+            if self.__mes in meses_31_dias:
+                max_dia = 31
+            elif self.__mes in meses_30_dias:
+                max_dia = 30
+            elif self.__mes == 2:
+            # febrero, revisamos bisiesto
+                if (self.__ano % 4 == 0) and ((self.__ano % 100 != 0) or (self.__ano % 400 == 0)):
+                    max_dia = 29
+                else:
+                    max_dia = 28
+            else:
+                raise DataError(f"El mes {self.__mes} no es válido")
+
+            if 1 <= dia <= max_dia:
+                self.__dia = dia
+            else:
+                raise DataError(f"El día {dia} no concuerda con los días del mes {self.__mes}")
+        else:
+            raise TypeError("El formato de día no es adecuado, debe ser un entero")
+    def getDia(self):
+        return self.__dia
+
+    ano = property(getAno,setAno)
+    mes = property(getMes,setMes)
+    dia = property(getDia,setDia)
 
     def __str__(self):
-        return f"{self.dia}/{self.mes}/{self.ano}"
+        return f"{self.__dia}/{self.__mes}/{self.__ano}"
 
 if __name__ == "__main__":
     try:
-        dia = int(input("Introduce o día: "))
-        mes = int(input("Introduce o mes: "))
-        ano = int(input("Introduce o ano: "))
-
-        data = Data(dia, mes, ano)
+        data = Data(2024,6,30)
         print("Data creada correctamente:", data)
-
     except DataError as e:
         print("Erro de formato na data:", e)
-
-    except ValueError:
-        print("Erro: debes introducir números enteiros")
-
-    except Exception as e:
-        print("Erro inesperado:", e)
+    except TypeError as i:
+        print("Erro:",i)
